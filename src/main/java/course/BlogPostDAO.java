d92fc852b9b161c3f6e528ad53d2a121e5de857b
John Sheehan <johnsheehan306@gmail.com>
2018-04-09 22:36:02 +0100

    3.2&3.3 done

---
+++ src/main/java/course/BlogPostDAO.java
@@ -0,0 +1,117 @@
+package course;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import org.bson.Document;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import static com.mongodb.client.model.Sorts.descending;
+import static com.mongodb.client.model.Sorts.orderBy;
+
+public class BlogPostDAO {
+    MongoCollection<Document> postsCollection;
+
+    public BlogPostDAO(final MongoDatabase blogDatabase) {
+        postsCollection = blogDatabase.getCollection("posts");
+    }
+
+    // Return a single post corresponding to a permalink
+    public Document findByPermalink(String permalink) {
+
+        // XXX HW 3.2,  Work Here
+        Document post = postsCollection.find(Filters.eq("title", permalink)).first();
+        return post;
+    }
+
+    // Return a list of posts in descending order. Limit determines
+    // how many posts are returned.
+    public List<Document> findByDateDescending(int limit) {
+
+        // XXX HW 3.2,  Work Here
+        // Return a list of DBObjects, each one a post from the posts collection
+        Iterable<Document> allPosts =  postsCollection.find().sort(orderBy(descending("date"))).limit(10);
+
+        List<Document> posts = new ArrayList<Document>();
+
+        for (Document post : allPosts) {
+            posts.add(post);
+        }
+
+        return posts;
+    }
+
+    public String addPost(String title, String body, List tags, String username) {
+
+        System.out.println("inserting blog entry " + title + " " + body);
+
+        String permalink = title.replaceAll("\\s", "_"); // whitespace becomes _
+        permalink = permalink.replaceAll("\\W", ""); // get rid of non alphanumeric
+        permalink = permalink.toLowerCase();
+
+
+        // XXX HW 3.2, Work Here
+        // Remember that a valid post has the following keys:
+        // author, body, permalink, tags, comments, date, title
+        //
+        // A few hints:
+        // - Don't forget to create an empty list of comments
+        // - for the value of the date key, today's datetime is fine.
+        // - tags are already in list form that implements suitable interface.
+        // - we created the permalink for you above.
+
+        // Build the post object and insert it
+        Document post = new Document();
+        post.append("title",title)
+                .append("permalink", permalink)
+                .append("date",new Date())
+                .append("body",body)
+                .append("tags", tags)
+                .append("comments", Arrays.asList())
+                .append("author",username);
+
+        postsCollection.insertOne(post);
+
+
+        return permalink;
+    }
+
+
+
+
+    // White space to protect the innocent
+
+
+
+
+
+
+
+
+    // Append a comment to a blog post
+    public void addPostComment(final String name, final String email, final String body,
+                               final String permalink) {
+
+        // XXX HW 3.3, Work Here
+        // Hints:
+        // - email is optional and may come in NULL. Check for that.
+        // - best solution uses an update command to the database and a suitable
+        //   operator to append the comment on to any existing list of comments
+
+        Document comment = new Document();
+                comment.append("author", name)
+                .append("body", body);
+        if ( email != "") {
+            comment.append("email", email);
+        }
+
+        postsCollection.updateOne(Filters.eq("permalink", permalink), Updates.push("comments", comment));
+    }
+}
