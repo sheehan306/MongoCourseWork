9911842b1a50ceaad9107059ac1636689d709a3c
jsheehan <jsheehan@cartrawler.com>
2018-04-09 16:54:35 +0100

    3.1 done

--- src/main/java/week3/HomeworkThreeOne.java
+++ src/main/java/week3/HomeworkThreeOne.java
@@ -1,11 +1,17 @@
 package week3;
 
 import com.mongodb.MongoClient;
+import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 import org.bson.Document;
+
+import javax.print.DocFlavor;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 /**
  * Created by deemoshea on 08/04/2018.
@@ -18,20 +24,23 @@ public class HomeworkThreeOne {
         MongoCollection<Document> collection = database.getCollection("students");
         System.out.println(collection.count());
 
-        Document myDoc = collection.find(Filters.in("scores")).first();
+        MongoCursor<String> c = collection.distinct("name", String.class ).iterator();
 
 
-        System.out.println(myDoc.toJson());
+        AggregateIterable<Document> cursor = collection.aggregate(Arrays.asList(new Document("$unwind","$scores"),
+                new Document("$match", new Document("scores.type","homework")),
+                new Document("$group", new Document("_id","$_id")
+                                .append("minscore", new Document("$min","$scores.score")))
+                ));
+        for (Document obj: cursor) {
+            System.out.println("Before: " + obj + "\n");
 
-//        MongoCursor<Document> cursor = collection.find().iterator();
-//        try {
-//            while (cursor.hasNext()) {
-//                System.out.println(cursor.next().toJson());
-//            }
-//        } finally {
-//            cursor.close();
-//        }
+            collection.updateOne(Filters.eq("_id", obj.get("_id")),Updates.pull("scores",new Document("score",obj.get("minscore"))));
 
+            System.out.println("After: " + obj + "\n");
+        }
+
+        mongoClient.close();
     }
 
 }
